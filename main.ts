b5b07e8cd16aae9b06862e8baa142dc29d79878a
Positivo <positivo@fpf.br>
2020-08-20 23:21:35 -0300

    send data to ThinkSpeak

--- main.ts
+++ main.ts
@@ -1,1 +1,114 @@
+namespace Wifi_ESP8266 {
 
+    let wifi_connected: boolean = false
+    let thingspeak_connected: boolean = false
+    let last_upload_successful: boolean = false
+    let serial_str: string = ""
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 100) {
+        serial.writeString(command + "\u000D\u000A")
+        basic.pause(wait)
+    }
+
+    // wait for certain response from ESP8266
+    function waitResponse(str: string): boolean {
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200) {
+                serial_str = serial_str.substr(serial_str.length - 200)
+            }
+            if (serial_str.includes(str)) {
+                result = true
+                break
+            }
+            if (input.runningTime() - time > 30000) break
+        }
+        return result
+    }
+
+    /**
+    * Initialize ESP8266 module and connect it to Wifi router
+    */
+    //% block="Initialize ESP8266|RX (Tx of micro:bit) %tx|TX (Rx of micro:bit) %rx|Baud rate %baudrate|Wifi SSID = %ssid|Wifi PW = %pw"
+    //% tx.defl=SerialPin.P0
+    //% rx.defl=SerialPin.P1
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_pw
+    export function connectWifi(tx: SerialPin, rx: SerialPin, baudrate: BaudRate, ssid: string, pwd: string) {
+        wifi_connected = false
+        thingspeak_connected = false
+        serial.redirect(
+            tx,
+            rx,
+            baudrate
+        )
+
+        sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+RST", 1000) // reset
+        sendAT("AT+CWMODE=1") // set to STA mode
+        // join wifi router
+        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pwd + "\"")
+        wifi_connected = waitResponse("OK")
+        //if (!result) control.reset()
+    }
+
+    /**
+    * Connect to ThingSpeak and upload data. It would not upload anything if it failed to connect to Wifi or ThingSpeak.
+    */
+    //% block="Upload data to ThingSpeak|URL/IP = %ip|Write API key = %write_api_key|Field 1 = %n1|Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
+    //% ip.defl=api.thingspeak.com
+    //% write_api_key.defl=your_write_api_key
+    export function connectThingSpeak(ip: string, write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
+        if (wifi_connected && write_api_key != "") {
+            thingspeak_connected = false
+            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",80", 0) // connect to website server
+            thingspeak_connected = waitResponse("OK")
+            basic.pause(100)
+            if (thingspeak_connected) {
+                last_upload_successful = false
+                let str: string = "GET /update?api_key=" + write_api_key + "&field1=" + n1 + "&field2=" + n2 + "&field3=" + n3 + "&field4=" + n4 + "&field5=" + n5 + "&field6=" + n6 + "&field7=" + n7 + "&field8=" + n8
+                sendAT("AT+CIPSEND=" + (str.length + 2))
+                sendAT(str, 0) // upload data
+                last_upload_successful = waitResponse("OK")
+                basic.pause(100)
+            }
+        }
+    }
+
+    /**
+    * Wait between uploads
+    */
+    //% block="Wait %delay ms"
+    //% delay.min=0 delay.defl=5000
+    export function wait(delay: number) {
+        if (delay > 0) basic.pause(delay)
+    }
+
+    /**
+    * Check if ESP8266 successfully connected to Wifi
+    */
+    //% block="Wifi connected ?"
+    export function isWifiConnected() {
+        return wifi_connected
+    }
+
+    /**
+    * Check if ESP8266 successfully connected to ThingSpeak
+    */
+    //% block="ThingSpeak connected ?"
+    export function isThingSpeakConnected() {
+        return thingspeak_connected
+    }
+
+    /**
+    * Check if ESP8266 successfully uploaded data to ThingSpeak
+    */
+    //% block="Last data upload successful ?"
+    export function isLastUploadSuccessful() {
+        return last_upload_successful
+    }
+
+}
